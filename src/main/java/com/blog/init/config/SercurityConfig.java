a095cab9806242195b2e183bc791461abbff09ba
wangzhaoning <1078674571@qq.com>
2019-04-09 23:40:00 +0800

    add security

---
+++ src/main/java/com/blog/init/config/SercurityConfig.java
@@ -0,0 +1,23 @@
+package com.blog.init.config;
+
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+	/**
+	 * ∞≤»´≈‰÷√¿‡
+	 * @author 10786
+	 *
+	 */
+public class SercurityConfig implements WebSecurityConfigurer {
+
+	@Override
+	public void init(SecurityBuilder builder) throws Exception {
+		
+	}
+
+	@Override
+	public void configure(SecurityBuilder builder) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
